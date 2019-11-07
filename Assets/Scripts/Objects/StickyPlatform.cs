f794955a1923c9f54b491028f370e5595732432a
Andrzej Zabski <jedrak32@gmail.com>
2019-11-07 23:54:52 +0100

    Sticky platform

---
+++ Assets/Scripts/Objects/StickyPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private Rigidbody2D _playerRb = null;
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+        _playerRb.gravityScale = 0.0f;
+        Debug.Log(_playerRb.gravityScale);
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _playerRb.gravityScale = 10;
+        _playerRb = null;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(_playerRb != null) Debug.Log(_playerRb.gravityScale);
+    }
+}
