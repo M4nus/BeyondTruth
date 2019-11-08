4b3e934cf69e67e5d127a7ee605f85d44b5995b5
Andrzej Zabski <jedrak32@gmail.com>
2019-11-08 16:04:25 +0100

    Bouncy an jumpy platforms

---
+++ Assets/Scripts/Objects/JumpPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlatform : MonoBehaviour
+{
+    private PlayerMovementTest _playerMovement;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _playerMovement = collision.gameObject.GetComponent<PlayerMovementTest>();
+        _playerMovement.jumpVelocity *= 2;
+        Debug.Log(_playerMovement.jumpVelocity);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _playerMovement.jumpVelocity /= 2;
+        Debug.Log(_playerMovement.jumpVelocity);
+        _playerMovement = null;
+        
+    }
+}
