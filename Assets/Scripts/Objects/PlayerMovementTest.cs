1cbb4fd585e58fe2a505e561dfcaa6d1915819f7
Krolag <dawidpaweloszek99@gmail.com>
2019-11-14 21:46:29 +0100

    Fixed a few bugs with CameraShake

--- Assets/Scripts/Objects/PlayerMovementTest.cs
+++ Assets/Scripts/Objects/PlayerMovementTest.cs
@@ -52,7 +52,6 @@ public class PlayerMovementTest : MonoBehaviour
 
 	void OnCollisionEnter2D(Collision2D collision) 
 	{
-        Debug.Log(collision.relativeVelocity.magnitude);
         if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
 		{
 		 	isOnGround = true;
