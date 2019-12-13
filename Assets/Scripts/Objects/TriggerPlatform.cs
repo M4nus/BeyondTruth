bb533b018fe88691baa88d8bb4a31d650385e3b1
Andrzej Zabski <jedrak32@gmail.com>
2019-12-13 11:35:20 +0100

    trigger platform

---
+++ Assets/Scripts/Objects/TriggerPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerPlatform : MonoBehaviour
+{
+    public UnityEvent e;
+
+
+    private void Start()
+    {
+        e.AddListener(Action);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        e.Invoke();
+    }
+
+    void Action()
+    {
+        Debug.Log("Trigger");
+    }
+}
---
+++ Assets/Scripts/Others/TriggerPlatrofmText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TriggerPlatrofmText : MonoBehaviour
+{
+    private bool _flag;
+
+
+    private void Start()
+    {
+        _flag = false;
+    }
+
+    private void Update()
+    {
+
+        gameObject.GetComponent<TextMeshProUGUI>().text = "State = " + _flag;
+      
+    }
+    public void Toggle()
+    {
+        _flag = !_flag;
+    }
+}
